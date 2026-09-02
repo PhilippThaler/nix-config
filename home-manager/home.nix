515217f2186d8c23390b949fb9ca0db9bcfe064c
Philipp Thaler <philipp@thaler.fyi>
2026-09-02 16:28:15 +0200

    Add default apps

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -236,9 +236,25 @@ in {
 
   # ── Dotfiles ──────────────────────────────────────────────────────
   xdg.configFile = {
-
     "gammastep/config.ini".source = ./dotfiles/gammastep/config.ini;
-    "mimeapps.list".source = ./dotfiles/mimeapps.list;
+  };
+
+  xdg.mimeApps.defaultApplications = {
+    "application/pdf" = "zathura.desktop";
+    "image/*" = "imv.desktop";
+    "text/*" = "nvim.desktop";
+    "inode/directory" = "yazi.desktop";
+    "x-scheme-handler/http" = "librewolf.desktop";
+    "x-scheme-handler/https" = "librewolf.desktop";
+    "x-scheme-handler/ftp" = "librewolf.desktop";
+    "x-scheme-handler/chrome" = "librewolf.desktop";
+    "text/html" = "librewolf.desktop";
+    "application/x-extension-htm" = "librewolf.desktop";
+    "application/x-extension-html" = "librewolf.desktop";
+    "application/x-extension-shtml" = "librewolf.desktop";
+    "application/xhtml+xml" = "librewolf.desktop";
+    "application/x-extension-xhtml" = "librewolf.desktop";
+    "application/x-extension-xht" = "librewolf.desktop";
   };
 
   home.file =
